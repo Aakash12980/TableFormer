7303ebc81cc87e9da047240fc2ee3edced1d44c0
Aakash Bhandari <Bhandariaakash9@gmail.com>
2022-03-11 23:03:57 +0545

    Model schema half

---
+++ model.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+import torch 
+from typing import List, Dict, Tuple
+
+class TableFormerModel(nn.Module):
+    def __init__(self) -> None:
+        super(TableFormerModel, self).__init__()
+
+    def forward(self):
+        pass
+
+    class ResNetModel():
+        def __init__(self) -> None:
+            
